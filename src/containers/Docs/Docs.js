a53ed779d9f89f1946e1ef4569002ab3c8aa93cc
xinpeng.gxp <xinpeng.gxp@alibaba-inc.com>
2018-09-12 20:09:56 +0800

    增加选择性打包demo

--- src/containers/Docs/Docs.js
+++ src/containers/Docs/Docs.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import Loadable from 'react-loadable';
 import _ from 'lodash';
 import { arraySum } from '@utils/utils';
 import './Docs.scss';
 
 class Docs extends React.Component {
+  state = {
+    ProdComponent: null
+  };
+
+  componentDidMount() {
+    if (process.env.NODE_ENV === 'production') {
+      const ProdComponent = Loadable({
+        loader: () => import(/* webpackChunkName: "prod-component" */ './ProductionOnly'),
+        loading() {
+          return <div />;
+        }
+      });
+      this.setState({ ProdComponent });
+    }
+  }
+
   render() {
+    let { ProdComponent } = this.state;
     const sum = arraySum([1, 3]);
     const b = _.sum([1, 3]);
     return (
@@ -13,6 +31,7 @@ class Docs extends React.Component {
         <div> 1 plus 3 equals {sum}</div>
         <br />
         <div>use _.sum, 1 plus 3 equals {b} too.</div>
+        {ProdComponent && <ProdComponent />}
       </div>
     );
   }
---
+++ src/containers/Docs/ProductionOnly.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+class Docs extends React.Component {
+  render() {
+    return <div style={{ color: 'red', marginTop: '30px' }}>这一行文字只会在生产环境下出现</div>;
+  }
+}
+export default Docs;
